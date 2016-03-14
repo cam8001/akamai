a502609fd27d172fbac2d57bcee50d1439dcb21b
Cameron Tod <cameron.tod@gmail.com>
2016-03-14 23:58:27 +0000

    back to simple

--- tests/drupal_ti/before/before_script.sh
+++ tests/drupal_ti/before/before_script.sh
@@ -16,24 +16,9 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 # Create the the module directory (only necessary for D7)
 # For D7, this is sites/default/modules
 # For D8, this is modules
+# mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 
-# Manually clone the dependencies for Composer Manager
-#git clone --depth 1 --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
-
-# Initialize composer manager
-#php "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH/composer_manager/scripts/init.php"
-# Now run it.
-#drush composer-update
-
-
-# Ensure the module is linked into the code base and enabled.
-# Note: This function is re-entrant.
-#drupal_ti_ensure_module_linked
-
-composer require akamai-open/edgegrid-client:0.4.3
-
-# Update composer
+# Install composer dependency
 cd "$DRUPAL_TI_DRUPAL_DIR"
-composer drupal-rebuild
-composer install --prefer-source
+composer require akamai-open/edgegrid-client "0.4.3"
