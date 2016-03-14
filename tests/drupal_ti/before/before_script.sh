cc57fe44c4e4a15f28b4a457e6064b80c61c4e57
Cameron Tod <cameron.tod@gmail.com>
2016-03-14 23:40:40 +0000

    Enabled composer_manager

--- tests/drupal_ti/before/before_script.sh
+++ tests/drupal_ti/before/before_script.sh
@@ -18,6 +18,17 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 # For D8, this is modules
 cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 
-# Install composer dependency
+# Manually clone the dependencies for Composer Manager
+git clone --depth 1 --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
+
+# Initialize composer manager
+php "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH/composer_manager/scripts/init.php"
+
+# Ensure the module is linked into the code base and enabled.
+# Note: This function is re-entrant.
+drupal_ti_ensure_module_linked
+
+# Update composer
 cd "$DRUPAL_TI_DRUPAL_DIR"
-composer require akamai-open/edgegrid-client "0.4.3"
+composer drupal-rebuild
+composer install --prefer-source
